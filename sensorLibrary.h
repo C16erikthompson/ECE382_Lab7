761be97337c6c8b93e90810a1c336764ea7b0d7a
C16erikthompson <C16erik.thompson@usafa.edu>
2014-12-07 21:52:18 -0700

    Create sensorLibrary.h

---
+++ sensorLibrary.h
@@ -0,0 +1,17 @@
+/*
+ * sensorLibrary.h
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: C16Erik.Thompson
+ */
+
+#ifndef SENSORLIBRARY_H_
+#define SENSORLIBRARY_H_
+
+#define		CLOSE				0x220;
+
+short getLeft();
+short getCenter();
+short getRight();
+
+#endif /* SENSORLIBRARY_H_ */
